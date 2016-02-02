46c42e1b3e9c6b8cddb55304c5a33b9f9a0eba93
abdul kalam <abdulkalam7051@gmail.com>
2016-02-02 14:01:08 +0530

    Adding TestPrj1

---
+++ TestPrj1/src/TestPkg1/TestCls1.java
@@ -0,0 +1,44 @@
+package TestPkg1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+
+public class TestCls1 {
+	
+	static String driverPath = "C:\\POCsAutomation\\workspace\\IEDriver32bit\\";
+	public WebDriver driver;
+	
+	@BeforeClass
+	public void setUp() {
+		System.out.println("*******************");
+		System.out.println("launching IE browser");
+		System.setProperty("webdriver.ie.driver", driverPath+"IEDriverServer.exe");
+		driver = new InternetExplorerDriver();
+		driver.manage().window().maximize();
+	}
+	
+	@Test
+	public void testGooglePageTitleInIEBrowser() {
+		driver.navigate().to("http://www.google.com");
+		String strPageTitle = driver.getTitle();
+		System.out.println("Page title: - "+strPageTitle);
+		Assert.assertTrue(strPageTitle.equalsIgnoreCase("Google"), "Page title doesn't match");
+	}
+
+	@AfterClass
+	public void tearDown() {
+		if(driver!=null) {
+			System.out.println("Closing IE browser");
+			driver.quit();
+		}
+	}
+
+	
+	
+
+}
